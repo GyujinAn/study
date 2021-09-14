718e973b67c1678a653333a603a1865f7a8b5ab5
Gyujin An <agj017@gmail.com>
2021-09-15 08:59:16 +0900

    create src/main/java/com/example/memberapi/model/Division.java

---
+++ src/main/java/com/example/memberapi/model/Division.java
@@ -0,0 +1,38 @@
+package com.example.memberapi.model;
+
+import jdk.jfr.Category;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author agj017@gmail.com
+ * @since 2021/09/15
+ */
+
+@Entity
+@Getter
+@Setter
+public class Division {
+    @Id
+    @GeneratedValue
+    @Column(name = "division_id")
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "id")
+    private List<Member> members = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private Division parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Division> child = new ArrayList<>();
+
+
+}
--- src/main/java/com/example/memberapi/model/Member.java
+++ src/main/java/com/example/memberapi/model/Member.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 /**
  * @author agj017@gmail.com
@@ -23,4 +24,12 @@ public abstract class Member {
     private Long id;
 
     private String name;
+
+    private LocalDateTime orderData;
+
+    @ManyToOne
+    @JoinColumn(name="division_id")
+    private Division division;
+
+
 }
