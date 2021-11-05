a75fefd24c19551ab2ecd0c7ddafa35cf66ba05e
Gyujin An <agj017@gmail.com>
2021-11-05 18:35:27 +0900

    create sso-study

---
+++ sso-study/resource3/src/test/java/com/example/resource3/Resource3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resource3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Resource3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
